d5d3c0ad32cf98619e69626f7fd3f4bf71b934dc
Maciej Moniuszko <moniuszko@gmail.com>
2019-09-11 12:28:47 +0200

    Modyfikacja głównej klasy i MailCounter

--- src/com/cyclequeue/MailCounter.java
+++ src/com/cyclequeue/MailCounter.java
@@ -2,4 +2,29 @@ package com.cyclequeue;
 
 public class MailCounter {
 
+	private int number; //numer stanowiska
+	private int hoursOfWork;//czas pracy
+	
+	public MailCounter(int number,int hours) {
+		this.number = number;
+		this.hoursOfWork = hours;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public int getHoursOfWork() {
+		return hoursOfWork;
+	}
+
+	public void setHoursOfWork(int hoursOfWork) {
+		this.hoursOfWork = hoursOfWork;
+	}
+	
+	
 }
--- src/com/cyclequeue/QueueMain.java
+++ src/com/cyclequeue/QueueMain.java
@@ -2,17 +2,31 @@ package com.cyclequeue;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
 
 public class QueueMain {
 
 	public static void main(String[] args) {
 		
+		int handlingTime; //czas ob³ugi okienka
+		int timeOfWork; //czas otwarcia w minutach
+		
 		List<Integer> lista = new LinkedList<>();
 		
-		for(int i=0;i<100;i++) {
-			lista.add(i+100);
+		for(int i=0;i<99;i++) {
+			lista.add(i+101);
 		}
 		
+		ListIterator<Integer> listIter = lista.listIterator();
+		
+		while(listIter.hasNext()) {
+			
+			
+			System.out.println(listIter.next());
+			//if(!listIter.hasNext()) {
+			//	listIter=lista.listIterator();
+			//}
+		}
 	}
 
 }
