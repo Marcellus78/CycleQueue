0e0eb2f6c5999c1617cd24bf59e7b8da9c4aba24
Maciej Moniuszko <moniuszko@gmail.com>
2019-09-11 11:28:27 +0200

    Nowa implementacja kolejki plus klas okienka pocztowego

---
+++ src/com/cyclequeue/QueueMain.java
@@ -0,0 +1,18 @@
+package com.cyclequeue;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class QueueMain {
+
+	public static void main(String[] args) {
+		
+		List<Integer> lista = new LinkedList<>();
+		
+		for(int i=0;i<100;i++) {
+			lista.add(i+100);
+		}
+		
+	}
+
+}
